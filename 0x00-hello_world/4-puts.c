3a90f5b4da402d25b0df94da4f0c53c2162737e1
Ololade Olanrewaju <lolareliza@gmail.com>
2022-04-07 10:19:04 -0700

    Updated Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/* main function is printing a string using the put function */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 	return (0);
 }
