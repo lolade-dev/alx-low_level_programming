464af4b94a562ccbab4669bcc1dbc81c366b6fe7
Ololade Olanrewaju <lolareliza@gmail.com>
2022-04-07 10:34:42 -0700

    Removed extra space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/* main function is printing a string using the put function */
+/*
+ * main - This is the entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
