3a59402ac56cde329efc99b31c3de84736876110
Ololade Olanrewaju <lolareliza@gmail.com>
2022-04-08 06:27:23 -0700

    Alphabets without q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,11 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch != 'q' || ch != 'e')
+		if (ch == 'q' || ch == 'e')
 		{
-			putchar(ch);
+			continue;
 		}
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
