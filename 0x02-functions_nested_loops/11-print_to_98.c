9aaa6611c70a6d6369e014bc01a66982cdc93200
Ololade <lolareliza@gmail.com>
2022-04-12 15:03:10 -0700

    Updated up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d", n);
+				printf("%d ", n);
 			}
 			else
 			{
@@ -27,7 +27,7 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d", n);
+				printf("%d ", n);
 			}
 			else
 			{
