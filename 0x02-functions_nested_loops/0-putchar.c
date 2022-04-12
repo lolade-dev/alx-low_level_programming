24b173b6eacc64eb08c9cce8cf4b306e1d602d52
Ololade <lolareliza@gmail.com>
2022-04-12 03:52:26 -0700

    Print character

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
