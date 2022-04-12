662430a7b1074168eb2408d8a9b6d09ebc786404
Ololade <lolareliza@gmail.com>
2022-04-12 15:15:48 -0700

    Updated up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,32 +8,26 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while (n <= 98)
+		for (; n <= 98; n++)
 		{
+			printf("%d", n);
+
 			if (n == 98)
-			{
-				printf("%d ", n);
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-			n++;
+				continue;
+			printf(", ");
 		}
+		printf("\n");
 	}
 	else
 	{
-		while (n >= 98)
+		for (; n >= 98; n--)
 		{
+			printf("%d", n);
+
 			if (n == 98)
-			{
-				printf("%d ", n);
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-			n--;
+				continue;
+			printf(", ");
 		}
+		printf("\n");
 	}
 }
