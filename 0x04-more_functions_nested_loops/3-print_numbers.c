ea63e4c83f4e720701ea107e19ab820d923f1138
Ololade <lolareliza@gmail.com>
2022-04-14 06:43:38 -0700

    Print from 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point to print u=numbers from 0 - 9
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; int <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
