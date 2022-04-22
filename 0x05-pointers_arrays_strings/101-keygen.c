b656e05e6e4af8053fc2db879f6288cc6f4ccaf1
Ololade Olanrewaju <49099449+lolade-dev@users.noreply.github.com>
2022-04-22 05:07:02 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,61 +8,10 @@
  */
 void keyGen(int N)
 {
-/*
- * int i, randomizer;
-
-	char numbers[], letter[], LETTER[], symbols[], password[N];
-
-	srand((unsigned int)(time(NULL)));
-
- Array of numbers
-	numbers[] = "0123456789";
-
-	 Array of lowercase alphabets
-	letter[] = "abcdefghijklmnoqprstuvwyzx";
-
-	 Array of uppercase alphabets
-	LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-
-	 Array of all the special symbols
-	symbols[] = "!@#$^&*?";
-
-	 To select the randomizer inside the loop
-	randomizer = rand() % 4;
-
-	for (i = 0; i < N; i++)
-	{
-		if (randomizer == 1)
-		{
-			password[i] = numbers[rand() % 10];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 2)
-		{
-			password[i] = symbols[rand() % 8];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 3)
-		{
-			password[i] = LETTER[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else
-		{
-	*		password[i] = letter[rand() % 26];
-	*		randomizer = rand() % 4;
-	*		printf("%c", password[i]);
-	*	}
-	}
-	*/
-	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
+	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\\"<>,.?/\\|";
+	int i;
 
 	srand(time(NULL));
-
-	int i;
 
 	for (i = 0; i < N; i++)
 	{
