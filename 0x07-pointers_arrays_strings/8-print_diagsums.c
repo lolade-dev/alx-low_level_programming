c854f14ca54ac513c5ca247dfc81b24a657b9219
Ololade <lolareliza@gmail.com>
2022-04-26 18:04:56 -0700

    Chessboard and 2d array

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Entry point
+ *
+ * @a: Array
+ * @size: size of the array
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		j += a[i];
+		k += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, ", j);
+	printf("%d\n", k);
+}
