04082b65239fb5e9682ee2ac47fd16fcd621e4e0
Ololade <lolareliza@gmail.com>
2022-04-28 04:34:48 -0700

    Recursive prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,3 +3,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int wrapper(int n, int guess);
+int is_prime_number(int n);
+int divide_number(int n, int guess);
