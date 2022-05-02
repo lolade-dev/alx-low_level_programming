db80936bcf090019b9bcb57e082b5dd3d19f0b52
Ololade <lolareliza@gmail.com>
2022-05-02 09:20:45 -0700

    Print the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - entry point
+ * @argc: counter
+ * @argv: array
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = argc - argc;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory is about argument counters and vectors
