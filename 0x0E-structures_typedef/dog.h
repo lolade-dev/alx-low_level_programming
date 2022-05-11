124e2111745588afdb44b60ef93421393fbd6b6e
Ololade <lolareliza@gmail.com>
2022-05-11 09:45:07 -0700

    Edit header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
+/**
+ * struct dog - structure for a basic dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: data structure for dog
+ */
 struct dog
 {
 	char *name;
