01f537a574a252853cadb4bdc4f5cf044c40f91e
Ololade <lolareliza@gmail.com>
2022-05-11 09:39:39 -0700

    Edit header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 struct dog
 {
 	char *name;
@@ -8,3 +11,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+#endif
