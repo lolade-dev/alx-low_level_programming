ac5724507352516aa4e4d19aa3a2971b9de13c99
Ololade Olanrewaju <Oolarenwaju@wragbysolutions.com>
2022-07-07 20:40:07 +0100

    Doubly linked lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_dlistint - function that prints all the elements of a list
+ * @h: input list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t sum = 0;
+
+    if (h == NULL)
+    {
+        return (sum);
+    }
+
+    while (h)
+    {
+        printf("%d\n", h->n)
+        sum++;
+        h = h->next;
+    }
+    return (sum);  
+    
+}
